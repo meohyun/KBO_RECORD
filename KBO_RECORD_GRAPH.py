6885e2eddd88cba1eeae37850c4e85895cd1adbc
meohyun24 <eogus1954@naver.com>
2022-07-04 22:31:58 +0900

    2022.07.04 22:31

--- KBO_RECORD_GRAPH.py
+++ KBO_RECORD_GRAPH.py
@@ -13,6 +13,7 @@ rc('font',family=font)
 df= pd.read_excel(f"{teamname}_기록.xlsx",sheet_name='포수')
 df1 = pd.read_excel(f"{teamname}_기록.xlsx",sheet_name='내야수')
 df2 = pd.read_excel(f"{teamname}_기록.xlsx",sheet_name='외야수')
+df3 = pd.read_excel(f"{teamname}_기록.xlsx",sheet_name='투수')
 
 data_concat= pd.concat([df,df1,df2],ignore_index=True)
 
@@ -20,24 +21,37 @@ for i in range(len(data_concat['AVG'])):
     if type(data_concat['AVG'][i]) == str:
         data_concat['AVG'][i] = 0.0
 
+for i in range(len(df3['ERA'])):
+    if type(df3['ERA'][i]) == str:
+        df3['ERA'][i] = 0.0
+
 data_concat = data_concat.drop(['Unnamed: 0'],axis=1)
 data_concat = data_concat.sort_values(by=['AVG'],axis=0)
-
+df3 = df3.sort_values(by=['ERA'],axis=0,ascending=False)
 
 x = data_concat['AVG'].values
 y = data_concat['선수명'].values
 
+x2 = df3['ERA'].values
+y2 = df3['선수명'].values
 
-plt.figure(figsize=(5,5))
-plt.barh(y,x,height=0.5)
-plt.title(f"{teamname} 야수 AVG")
+def bar_graph(x,y,sheet_name):
+    plt.figure(figsize=(5,5))
+    plt.barh(y,x,height=0.5)
+    if sheet_name == '투수':
+        plt.title(f"{teamname} {sheet_name} ERA")
+    else : 
+        plt.title(f"{teamname} {sheet_name} AVG")
 
-for i, v in enumerate(x):
-    plt.text(v, y[i], x[i],                 # 좌표 (x축 = v, y축 = y[0]..y[1], 표시 = y[0]..y[1])
-             fontsize = 9, 
-             color='blue',
-             horizontalalignment='left',  # horizontalalignment (left, center, right)
-             verticalalignment='center')    # verticalalignment (top, center, bottom)
-plt.show()
+    for i, v in enumerate(x):
+        plt.text(v, y[i], x[i],                 # 좌표 (x축 = v, y축 = y[0]..y[1], 표시 = y[0]..y[1])
+                fontsize = 9, 
+                color='blue',
+                horizontalalignment='left',  # horizontalalignment (left, center, right)
+                verticalalignment='center')    # verticalalignment (top, center, bottom)
+    plt.show()
+
+# bar_graph(x2,y2,'투수')
+bar_graph(x,y,'야수')
 
 
